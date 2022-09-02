9c9bda597311a72b45e84ae690d162b19ffd2efc
Landon Sutherland <sutherlandon@gmail.com>
2022-09-02 01:18:23 -0600

    Added advancement paths to the constants and created a book report style layout

--- config/constants.js
+++ config/constants.js
@@ -1,3 +1,4 @@
+/* eslint-disable quote-props */
 import FoxesLogo from '../images/foxesLogo.png';
 import HawksLogo from '../images/hawksLogo.png';
 import MountainLionsLogo from '../images/mountainLionsLogo.png';
@@ -11,14 +12,18 @@ export const EVENT_TYPES = [
   'Other',
 ];
 
+export const ADVANCEMENT = {
+  'Heritage': { core: 2, elective: 1, htt: 1 },
+  'Hobbies': { core: 1, elective: 2, htt: 1 },
+  'Life Skills': { core: 3, elective: 1, htt: 1 },
+  'Outdoor Skills': { core: 3, elective: 1, htt: 1 },
+  'Science/Tech': { core: 2, elective: 1, htt: 1 },
+  'Sports/Fitness': { core: 2, elective: 1, htt: 1 },
+  'Values': { core: 2, elective: 1, htt: 1 },
+};
+
 export const BRANCHES = [
-  'Heritage',
-  'Hobbies',
-  'Life Skills',
-  'Outdoor Skills',
-  'Science/Tech',
-  'Sports/Fitness',
-  'Values',
+  ...Object.keys(ADVANCEMENT),
   'Award',
   'Board',
   'Camp',
@@ -28,58 +33,19 @@ export const BRANCHES = [
 ];
 
 export const BRANCH_COLORS = {
-  Award: {
-    b: 'purple',
-    t: 'white',
-  },
-  Board: {
-    b: 'lightgray',
-    t: 'black',
-  },
-  Camp: {
-    b: 'yellow',
-    t: 'black',
-  },
-  'Day Hike': {
-    b: 'yellow',
-    t: 'black',
-  },
-  Fundraiser: {
-    b: 'limegreen',
-    t: 'black',
-  },
-  General: {
-    b: 'lightgray',
-    t: 'black',
-  },
-  Heritage: {
-    b: '#896400',
-    t: 'white',
-  },
-  Hobbies: {
-    b: '#212121',
-    t: 'white',
-  },
-  'Life Skills': {
-    b: '#8c1006',
-    t: 'white',
-  },
-  'Outdoor Skills': {
-    b: '#005283',
-    t: 'white',
-  },
-  'Science/Tech': {
-    b: '#ffb130',
-    t: 'black',
-  },
-  'Sports/Fitness': {
-    b: '#425b21',
-    t: 'white',
-  },
-  Values: {
-    b: '#c5171c',
-    t: 'white',
-  },
+  'Heritage': { b: '#896400', t: 'white' },
+  'Hobbies': { b: '#212121', t: 'white' },
+  'Life Skills': { b: '#8c1006', t: 'white' },
+  'Outdoor Skills': { b: '#005283', t: 'white' },
+  'Science/Tech': { b: '#ffb130', t: 'black' },
+  'Sports/Fitness': { b: '#425b21', t: 'white' },
+  'Values': { b: '#c5171c', t: 'white' },
+  'Award': { b: 'purple', t: 'white' },
+  'Board': { b: 'lightgray', t: 'black' },
+  'Camp': { b: 'yellow', t: 'black' },
+  'Day Hike': { b: 'yellow', t: 'black' },
+  'Fundraiser': { b: 'limegreen', t: 'black' },
+  'General': { b: 'lightgray', t: 'black' },
 };
 
 export const PATROLS = [
@@ -91,17 +57,17 @@ export const PATROLS = [
 ];
 
 export const PATROL_COLORS = {
-  Foxes: '#b97c38',
-  Hawks: '#eab71b',
+  'Foxes': '#b97c38',
+  'Hawks': '#eab71b',
   'Mountain Lions': '#cea54a',
-  Navigators: '#bbbdbd',
-  Adventurers: '#8ec8e7',
+  'Navigators': '#bbbdbd',
+  'Adventurers': '#8ec8e7',
 };
 
 export const PATROL_LOGOS = {
-  Foxes: FoxesLogo,
-  Hawks: HawksLogo,
+  'Foxes': FoxesLogo,
+  'Hawks': HawksLogo,
   'Mountain Lions': MountainLionsLogo,
-  Adventurers: AdventurersLogo,
-  Navigators: NavigatorsLogo,
+  'Adventurers': AdventurersLogo,
+  'Navigators': NavigatorsLogo,
 };
---
+++ pages/reports.js
@@ -0,0 +1,118 @@
+import {
+  Table,
+  TableBody,
+  TableHead,
+  TableRow,
+  TableCell,
+} from '@mui/material';
+
+import {
+  ADVANCEMENT,
+  BRANCH_COLORS,
+} from '../config/constants';
+
+function AdvRow({ label }) {
+  return (
+    <>
+      <TableRow>
+        <TableCell
+          rowSpan={3}
+          sx={{
+            borderTop: 3,
+            borderTopColor: 'gray',
+            borderBottom: 3,
+            borderBottomColor: 'gray',
+          }}
+        >
+          {label}
+        </TableCell>
+        <TableCell
+          sx={{
+            borderTop: 3,
+            borderTopColor: 'gray',
+          }}
+        >
+          Date
+        </TableCell>
+        {Object.keys(ADVANCEMENT).map((branch) => (
+          <TableCell key={branch} sx={{
+            background: BRANCH_COLORS[branch].b,
+            color: BRANCH_COLORS[branch].t,
+            whiteSpace: 'nowrap',
+            minWidth: 80,
+          }}>
+          </TableCell>
+        ))}
+      </TableRow>
+      <TableRow>
+        <TableCell>Step</TableCell>
+        {Object.keys(ADVANCEMENT).map((branch) => (
+          <TableCell key={branch} sx={{
+            background: BRANCH_COLORS[branch].b,
+            color: BRANCH_COLORS[branch].t,
+            whiteSpace: 'nowrap',
+            minWidth: 80,
+          }}>
+          </TableCell>
+        ))}
+      </TableRow>
+      <TableRow>
+        <TableCell
+          sx={{
+            borderBottom: 3,
+            borderBottomColor: 'gray',
+          }}
+        >
+          TG Inits
+        </TableCell>
+        {Object.keys(ADVANCEMENT).map((branch) => (
+          <TableCell key={branch} sx={{
+            background: BRANCH_COLORS[branch].b,
+            color: BRANCH_COLORS[branch].t,
+            whiteSpace: 'nowrap',
+            minWidth: 80,
+            borderBottom: 3,
+            borderBottomColor: 'gray',
+          }}>
+          </TableCell>
+        ))}
+      </TableRow>
+    </>
+  );
+}
+
+function BookReport(props) {
+  return (
+    <Table size='small'>
+      <TableHead>
+        <TableRow>
+          <TableCell></TableCell>
+          <TableCell></TableCell>
+          {Object.keys(ADVANCEMENT).map((branch) => (
+            <TableCell key={branch} sx={{
+              background: BRANCH_COLORS[branch].b,
+              borderBottom: 3,
+              borderBottomColor: 'gray',
+              color: BRANCH_COLORS[branch].t,
+              whiteSpace: 'nowrap',
+              minWidth: 80,
+            }}>
+              {branch}
+            </TableCell>
+          ))}
+        </TableRow>
+        <AdvRow label='Core'></AdvRow>
+        <AdvRow label='Core'></AdvRow>
+        <AdvRow label='Core'></AdvRow>
+        <AdvRow label='Elective'></AdvRow>
+        <AdvRow label='Elective'></AdvRow>
+        <AdvRow label='HTT'></AdvRow>
+      </TableHead>
+
+      <TableBody>
+      </TableBody>
+    </Table>
+  );
+};
+
+export default BookReport;
